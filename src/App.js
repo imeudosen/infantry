19676c1f77e469e82ec655f6ee69d726a004a419
imeudosen <imeudosen@gmail.com>
2020-09-12 06:47:14 -0400

    framework setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: '',
+			lastName: '',
+			password: '',
+			gender: '',
+			formErrors: {
+				firstName: '',
+				lastName: 'd',
+				password: '',
+				// password: 'minimum 3 characaters required',
+				gender: '',
+			},
+			invalid: false,
+		};
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+  };
+  
+
+
+
+
+  
+	render() {
+		return <h1>Hello, {this.props.name}</h1>;
+	}
 }
 
 export default App;
