d8feaf4c2945729a9cd69057199040bc940652d7
imeudosen <imeudosen@gmail.com>
2020-09-12 08:49:04 -0400

    nav-bar-added

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+
+//giving the links called 'navStyle' a color
+function Nav() {
+	const navStyle = {
+		color: 'white',
+	};
+
+	return (
+		<nav>
+			<h3>Life Planner</h3>
+			<ul className='nav-links'>
+				<Link style={navStyle} to='/Signin'>
+					<li>Sin In</li>
+				</Link>
+				<Link style={navStyle} to='/Register'>
+					<li>Register</li>
+				</Link>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
